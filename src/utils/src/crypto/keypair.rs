1810996e6dfe16a4312889633af8dc301195000f
Pankaj Mendki <pankajm@talentica.com>
2020-01-09 13:50:14 +0530

    Fix compile issue

--- src/utils/src/crypto/keypair.rs
+++ src/utils/src/crypto/keypair.rs
@@ -56,14 +56,22 @@ impl Verify<PublicKeyType> for PublicKey {
 #[cfg(test)]
 mod tests {
     #[test]
+    fn test_generate() {
+        use super::*;
+        let kp = Keypair::generate();
+        println!("pub : {:?}", hex::encode(kp.public().encode()));
+        println!("secret : {:?}", hex::encode(Keypair::secret(&kp)));
+        assert_eq!(true, true);
+    }
+    #[test]
     fn test_secret() {
         use super::*;
-        let s = "97ba6f71a5311c4986e01798d525d0da8ee5c54acbf6ef7c3fadd1e2f624442f";
+        let s = "728421f50913fefe04724bfad213624cb47ce9666445d2148bdbae0ba38e1884";
         let mut secret_bytes = hex::decode(s).expect("invalid secret");
         let kp = Keypair::generate_from(secret_bytes.as_mut_slice());
         assert_eq!(
             hex::encode(kp.public().encode()),
-            "2c8a35450e1d198e3834d933a35962600c33d1d0f8f6481d6e08f140791374d0"
+            "a986f7e76ec53d46d78f69fb2d76ae9378b439a6db43afba7a6761a01ccf3a7d"
         );
     }
     #[test]
@@ -72,7 +80,10 @@ mod tests {
         let s = "97ba6f71a5311c4986e01798d525d0da8ee5c54acbf6ef7c3fadd1e2f624442f";
         let mut secret_bytes = hex::decode(s).expect("invalid secret");
         let kp = Keypair::generate_from(secret_bytes.as_mut_slice());
-        println!("pub : {:?}", hex::encode(Keypair::public(&kp).encode()));
+        println!(
+            "pub : {:?}",
+            hex::encode(kp.public().encode())
+        );
         // println!("secrete {:?}", hex::encode(kp.secret().as_ref()));
         let sign = Keypair::sign(&kp, b"Hello World");
         assert_eq!(
