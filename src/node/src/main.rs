b99c422f525f7869a92f4f99624a2c34ec963f70
gaurav agarwal <gaurav.agarwal@talentica.com>
2020-02-07 11:55:53 +0530

    message and topics updated in p2plib

--- src/node/src/main.rs
+++ src/node/src/main.rs
@@ -11,18 +11,22 @@ use utils::configreader::Configuration;
 use p2plib::messages::Message;
 use p2plib::messages::*;
 use p2plib::simpleswarm::SimpleSwarm;
-use libp2p::PeerId;
+use libp2p::{PeerId, identity::PublicKey};
 
 fn main() {
     let config: &Configuration = &configreader::GLOBAL_CONFIG;
-    let peer_id = config.node.public.into_peer_id();
-    let peer_id = PeerId::from_public_key(config.node.public.clone());
+    let pk: PublicKey = PublicKey::Ed25519(config.node.public.clone());
+    let peer_id = PeerId::from_public_key(pk);
     println!("peer id = {:?}", peer_id);
     let mut swarm = SimpleSwarm::new();
     swarm.topic_list.push(String::from(BlockCreate::TOPIC));
     swarm
         .topic_list
         .push(String::from(TransactionCreate::TOPIC));
+    swarm.topic_list.push(String::from(SignedBlock::TOPIC));
+    swarm
+        .topic_list
+        .push(String::from(SignedTransaction::TOPIC));
     swarm.process(peer_id, config);
 
     let transaction_pool: TransactionPool = TransactionPool::new();
--- src/p2plib/src/messages.rs
+++ src/p2plib/src/messages.rs
@@ -1,6 +1,9 @@
 use super::constants;
 use libp2p::floodsub::{self, Topic, TopicBuilder};
-use serde::{Deserialize, Serialize};
+// use serde::{Deserialize, Serialize};
+use utils::serializer::{Deserialize, Serialize};
+pub use schema::transaction::{SignedTransaction, Txn};
+pub use schema::block::{SignedBlock, SignedBlockTraits};
 
 pub trait Message {
     const TOPIC: &'static str;
@@ -39,6 +42,14 @@ impl Message for TransactionCreate {
     }
 }
 
+impl Message for SignedTransaction{
+    const TOPIC: &'static str = "signed_transaction";
+    const MODULE_TOPIC: &'static str = constants::NODE;
+    fn handler(&self) {
+        println!("i am txn create");
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct BlockCreate {
     pub height: i64,
@@ -53,10 +64,18 @@ impl Message for BlockCreate {
     }
 }
 
+impl Message for SignedBlock {
+    const TOPIC: &'static str = "signed_block";
+    const MODULE_TOPIC: &'static str = constants::NODE;
+    fn handler(&self) {
+        println!("i am blockcreate");
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 pub enum NodeMessageTypes {
-    TransactionCreate(TransactionCreate),
-    BlockCreate(BlockCreate),
+    SignedTransactionEnum(SignedTransaction),
+    SignedBlockEnum(SignedBlock),
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -75,8 +94,8 @@ pub enum MessageTypes {
 impl From<NodeMessageTypes> for Topic {
     fn from(msg: NodeMessageTypes) -> Topic {
         match msg {
-            TransactionCreate => TopicBuilder::new(TransactionCreate::TOPIC).build(),
-            BlockCreate => TopicBuilder::new(BlockCreate::TOPIC).build(),
+            SignedTransactionEnum => TopicBuilder::new(SignedTransaction::TOPIC).build(),
+            SignedBlockEnum => TopicBuilder::new(SignedTransaction::TOPIC).build(),
         }
     }
 }
